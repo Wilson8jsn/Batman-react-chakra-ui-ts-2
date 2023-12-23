d5744480f2b72240eff9405bac07a43865bd3b0a
Pablo Lucero <115800617+Wilson8jsn@users.noreply.github.com>
2023-12-22 19:17:07 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 
 import React from 'react';
-import AppCard from './components/card'; 
+import AppCard from './components/Batman'; 
 
-<<<<<<< HEAD
-const MainApp: React.FC = () => {
-  return (
-    <div>
-      
-   
-      <App />
-    </div>
-  );
-};
-=======
+
 const App: React.FC = () => (
   <div>
     <AppCard />
   </div>
 );
->>>>>>> 0e34d68b485784cc3c5096c7b99dfab994b6ca0d
 
 export default App;
